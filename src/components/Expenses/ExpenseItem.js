2c640d76a083e94c99cec63ff6165da30914f609
Eetu <105866472+awonfs@users.noreply.github.com>
2022-07-19 21:29:59 +0300

    Add files via upload

---
+++ src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./ExpenseItem.css";
+import Card from "../UI/Card";
+import ExpenseDate from "./ExpenseDate";
+
+// This function creates a div that contains information about the expenses
+
+const ExpenseItem = (props) => {
+  return (
+    <li>
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">{props.amount}€</div>
+      </div>
+    </Card>
+    </li>
+  );
+};
+
+export default ExpenseItem;
